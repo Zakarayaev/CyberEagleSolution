f0b43be105a3f5a842734122889a164ee3135348
alimu <alimuhammadzakar@gmail.com>
2024-12-16 20:03:20 +0300

    Add project files.

---
+++ CyberEagle.UI/CyberEagle.UI/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace CyberEagle.UI.Views
+{
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CyberEagle.UI/CyberEagle.UI/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace CyberEagle.UI.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
